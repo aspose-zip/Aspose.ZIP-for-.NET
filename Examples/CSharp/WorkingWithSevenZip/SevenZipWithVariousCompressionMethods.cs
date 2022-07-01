d227670b028c277ded453887a70b7f3c390bbc15
Eugene85 <shashkov85@mail.ru>
2022-07-02 03:01:09 +0400

    Introduced different compression methods for 7z archive.

---
+++ Examples/CSharp/WorkingWithSevenZip/SevenZipWithVariousCompressionMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Aspose.Zip.Saving;
+using Aspose.Zip.SevenZip;
+
+namespace Aspose.ZIP.Examples.WorkingWithSevenZip
+{
+    public class SevenZipWithVariousCompressionMethods
+    {
+        public static void Run()
+        {
+            string dataDir = RunExamples.GetDataDir_Data();
+
+            //ExStart: SevenZipWithVariousCompressionMethods
+            
+            //LZMA2
+            using (SevenZipArchive archive = new SevenZipArchive(new SevenZipEntrySettings(new SevenZipLZMA2CompressionSettings())))
+            {
+                archive.CreateEntries(dataDir);
+                archive.Save("SevenZip_lzma2.7z");
+            }
+            
+            //BZip2
+            using (SevenZipArchive archive = new SevenZipArchive(new SevenZipEntrySettings(new SevenZipBZip2CompressionSettings())))
+            {
+                archive.CreateEntries(dataDir);
+                archive.Save("SevenZip_bzip2.7z");
+            }
+            
+            //Store, no compression
+            using (SevenZipArchive archive = new SevenZipArchive(new SevenZipEntrySettings(new SevenZipStoreCompressionSettings())))
+            {
+                archive.CreateEntries(dataDir);
+                archive.Save("SevenZip_store.7z");
+            }
+            
+            //ExEnd: SevenZipWithVariousCompressionMethods
+            Console.WriteLine("Successfully Created a Seven Zip Files");
+        }
+    }
+}
