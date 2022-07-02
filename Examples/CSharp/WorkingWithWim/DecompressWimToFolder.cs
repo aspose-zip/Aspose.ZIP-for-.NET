41a90844ee7be03f3b78ff2234af0d01bc77eed0
Eugene85 <shashkov85@mail.ru>
2022-07-02 04:16:07 +0400

    Introduced wim extraction.

---
+++ Examples/CSharp/WorkingWithWim/DecompressWimToFolder.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Aspose.Zip.Wim;
+
+namespace Aspose.ZIP.Examples.WorkingWithWim
+{
+    public class DecompressWimToFolder
+    {
+        public static void Run()
+        {
+            string dataDir = RunExamples.GetDataDir_Data();
+
+            //ExStart: DecompressWimArchive
+            using (FileStream fs = File.OpenRead(dataDir + "corpus.wim"))
+            {
+                using (WimArchive archive = new WimArchive(fs))
+                {
+                    archive.Images[0].ExtractToDirectory(dataDir + "DecompressWim_out");
+                }
+            }
+            //ExEnd: DecompressWimArchive
+        }
+    }
+}
