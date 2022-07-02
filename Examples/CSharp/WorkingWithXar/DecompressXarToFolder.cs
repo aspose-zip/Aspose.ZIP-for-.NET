906815120ee0da0626ee92f4e537b1fe3aa1f421
Eugene85 <shashkov85@mail.ru>
2022-07-02 04:23:10 +0400

    Introduced xar extraction.

---
+++ Examples/CSharp/WorkingWithXar/DecompressXarToFolder.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Aspose.Zip.Xar;
+
+namespace Aspose.ZIP.Examples.WorkingWithXar
+{
+    public class DecompressXarToFolder
+    {
+        public static void Run()
+        {
+            string dataDir = RunExamples.GetDataDir_Data();
+
+            //ExStart: DecompressXarArchive
+            using (FileStream fs = File.OpenRead(dataDir + "sample.xar"))
+            {
+                using (XarArchive archive = new XarArchive(fs))
+                {
+                    archive.ExtractToDirectory(dataDir + "DecompressXar_out");
+                }
+            }
+            //ExEnd: DecompressXarArchive
+        }
+    }
+}
